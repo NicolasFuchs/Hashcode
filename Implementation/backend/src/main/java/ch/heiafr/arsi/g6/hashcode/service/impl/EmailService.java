7073b0d7a8a06a4eb39bc6eca2f6593490a9be79
Jonathan Rial <jonathan.rial@edu.hefr.ch>
2018-05-24 10:43:46 +0200

    Add template for email

--- Implementation/backend/src/main/java/ch/heiafr/arsi/g6/hashcode/service/impl/EmailService.java
+++ Implementation/backend/src/main/java/ch/heiafr/arsi/g6/hashcode/service/impl/EmailService.java
@@ -5,32 +5,39 @@ import ch.heiafr.arsi.g6.hashcode.service.IEmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
-
-import javax.mail.MessagingException;
-import javax.mail.internet.MimeMessage;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 @Service
 public class EmailService implements IEmailService {
 
+  private static final String BASE_URL = "http://localhost:4200/";
+
   private final JavaMailSender javaMailSender;
+  private final TemplateEngine templateEngine;
 
   @Autowired
-  public EmailService(JavaMailSender javaMailSender) {
+  public EmailService(JavaMailSender javaMailSender, TemplateEngine templateEngine) {
     this.javaMailSender = javaMailSender;
+    this.templateEngine = templateEngine;
   }
 
   @Override
   public void sendVerificationEmail(Account account) {
-    MimeMessage message = javaMailSender.createMimeMessage();
-    MimeMessageHelper helper = new MimeMessageHelper(message);
-    try {
-      helper.setTo("jonathan.rial@edu.hefr.ch");
-      helper.setText("How are you?");
-      helper.setSubject("Hi");
-    } catch (MessagingException e) {
-      e.printStackTrace();
-    }
-    javaMailSender.send(message);
+    Context context = new Context();
+    context.setVariable("name", "Hello " + account.getFirstname() + " " + account.getLastname() + " !");
+    context.setVariable("validationHash", BASE_URL + "validation/" + "aksdjhfalkjsdakjhdslfkja");
+    String content = templateEngine.process("EmailAccountValidation", context);
+    MimeMessagePreparator messagePreparator =
+        mimeMessage -> {
+          MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+          messageHelper.setFrom("Hashcode G6 <hashcode.g6@gmail.com>");
+          messageHelper.setTo(account.getEmail());
+          messageHelper.setSubject("Validation de votre compte Hashcode");
+          messageHelper.setText(content, true);
+        };
+    javaMailSender.send(messagePreparator);
   }
 }
